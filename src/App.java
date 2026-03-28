88633091b6cc3b0aea1b11af3d1846e3ffcdaded
yvegaga <yvegaga@ufpso.edu.co>
2026-03-28 08:24:48 -0500

    termine parcial

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+    
+        System.out.println("ingrese su nombre");
+        System.out.println("ingrese su numero de identificacion");
+        System.out.println("ingrese su puntaje obtenido");
+
+if 60 > 74 
+
+   System.out.println("el puntaje es aceptable");
+   else 
+    System.out.println("el puntaje es insuficiente");
+
+     if 75 > 89
+
+     System.out.println("el puntaje es desempeño sobresaliente");
+     
+     else System.out.println("el puntaje es un buen desempeño ");
+       
+     if 0>= 100 
+
+      System.out.println(" el puntaje no es valido con la escala de puntos");
     }
-}
+}     
